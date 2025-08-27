ce8bacbeaf25bf5a697ad9b2eed09fa9cceec17a
Jeet Das <126141713+jeetOnGit@users.noreply.github.com>
2025-08-27 21:05:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.use(cors({
   origin: [
         "http://localhost:5173",
         "http://localhost:5174",
+    "https://easyibrary.netlify.app"
         
     ], 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
