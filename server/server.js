08c43e30e8c82432297849fda5afcdb87431a8ad
Jeet Das <contactwithjeeet@gmail.com>
2025-08-27 20:53:50 +0530

    build command

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ app.use(express.json())
 app.use(cors({
   origin: [
         "http://localhost:5173",
-        "http://localhost:5174"  
+        "http://localhost:5174",
+        
     ], 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,                // allow cookies / auth headers
